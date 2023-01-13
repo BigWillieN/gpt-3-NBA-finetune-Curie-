c2f76aa483179fa97c616ca51f8ffa3a16a247d9
BerkTheHun <69094722+BigWillieN@users.noreply.github.com>
2023-01-13 20:14:58 +0100

    Add files via upload
    CSV files to be fed to GPT-3, prepared files to be used for fine tuning/training our custom model. NBAFINETUNE.py hold gui and training setup code.

---
+++ NBAFINETUNE.py
@@ -0,0 +1,45 @@
+import openai
+import tkinter as tk
+
+# Set your OpenAI API key
+openai.api_key = "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"
+
+# Create the GUI window
+root = tk.Tk()
+root.title("ChatGPT NBA FineTune GUI")
+
+# Create the input prompt field
+prompt_label = tk.Label(root, text="Enter your prompt:")
+prompt_label.pack()
+prompt_field = tk.Entry(root)
+prompt_field.pack()
+
+# Create the result display area
+result_label = tk.Label(root, text="Result:")
+result_label.pack()
+result_text = tk.Text(root)
+result_text.pack()
+
+# Define the function to handle the "Enter" button press
+def on_enter():
+    # Get the prompt text from the input field
+    prompt = prompt_field.get()
+    
+    # Use the OpenAI API to generate a completion for the prompt
+    response = openai.Completion.create(
+        model="curie:ft-politecnico-di-torino-2023-01-13-19-04-16",
+        prompt=prompt
+    )
+    
+    # Get the generated text from the API response
+    generated_text = response["choices"][0]["text"]
+    
+    # Insert the generated text into the result display area
+    result_text.insert("1.0", generated_text)
+
+# Create the "Enter" button
+enter_button = tk.Button(root, text="Enter", command=on_enter)
+enter_button.pack()
+
+# Run the GUI
+root.mainloop()
